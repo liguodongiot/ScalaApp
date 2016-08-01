65f897d9c7940c821cbff96a025a1a098c128d7a
liguodong <liguodongiot@163.com>
2016-08-01 20:20:57 +0800

    add

--- ScalaStudy/ScalaCookbook/src/cb011/UseRange.scala
+++ ScalaStudy/ScalaCookbook/src/cb011/UseRange.scala
@@ -5,4 +5,16 @@ package cb011
   */
 object UseRange {
 
+  println(1 to 10)
+
+  println(1 until 10)
+
+  println(1 to 10 by 2)
+
+  println( 'a' to 'c')
+
+  //you can use ranges to create and populate sequences:
+  val x = (1 to 10).toList
+  println(x)
+  
 }
